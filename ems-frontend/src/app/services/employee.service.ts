8a1c0dc4324ad862a9e522b8a0c8a9ce52b0cabf
Fahad <fahad630321@gmail.com>
2025-12-22 19:15:12 +0530

    Add map operator to employee service imports

--- ems-frontend/src/app/services/employee.service.ts
+++ ems-frontend/src/app/services/employee.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 import { environment } from '../environments/environment'; 
 import {
   Employee,
